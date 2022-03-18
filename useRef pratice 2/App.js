dd96aefe1feaff11075da0aa75a881e3061ef647
woosang <woosang3078@hanmail.net>
2022-03-18 22:46:35 +0900

    useRef pratice code 2

---
+++ useRef pratice 2/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useRef } from 'react';
+import UserList from './UserList';
+
+
+
+function App() {
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+
+
+    nextId.current += 1;
+  };
+
+  return <UserList users={users} />;
+}
+
+export default App;
