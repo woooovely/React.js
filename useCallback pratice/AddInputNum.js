5b926cf350cd910eb11f9682e9a390f35d122ceb
woosang <woosang3078@hanmail.net>
2022-03-31 14:41:36 +0900

    react useCallback pratice code

---
+++ useCallback pratice/AddInputNum.js
@@ -0,0 +1,44 @@
+import React, { Fragment, useState, useMemo, useCallback } from 'react';
+
+
+const getTotal = numbers => {
+    let total = 0;
+
+    console.log('getTotal() 호출');
+
+    numbers.forEach(value => {
+        total += value;
+    });
+
+    return total;
+};
+
+const AddInputNum = () => {
+    const [numbers, setNumbers] = useState([]);
+    const [input, setInput] = useState('');
+
+    const onChange = useCallback((e) => {
+        setInput(e.target.value);
+    }, []);
+
+    const onInsert = useCallback((e) => {
+        const nextNumber = numbers.concat(parseInt(input));
+
+        setNumbers(nextNumber);
+        setInput('');
+    }, [input, numbers]);
+
+    const total = useMemo(() => getTotal(numbers), [numbers])
+
+    return (
+        <Fragment>
+            <input value={input} onChange={onChange} />
+            <button onClick={onInsert}>입력</button>
+            <div>
+                <span>총합: </span>{total}
+            </div>
+        </Fragment>
+    );
+};
+
+export default AddInputNum;
---
+++ useCallback pratice/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import './App.css';
+import AddInputNum from './AddInputNum';
+
+const App = () => {
+  return <AddInputNum />;
+}
+
+export default App;
