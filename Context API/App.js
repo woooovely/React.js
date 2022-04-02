7eec03a35f1b157b14a7582b47de3b1725f2a819
woosang <woosang3078@hanmail.net>
2022-04-02 14:45:17 +0900

    react Context API: change color component

---
+++ Context API/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import ColorBox from './components/ColorBox';
+import { ColorProvider } from './contexts/color';
+import SelectColors from './components/SelectColor';
+
+const App = () => {
+  return (
+    <ColorProvider>
+      <div>
+        <SelectColors />
+        <ColorBox />
+      </div>
+    </ColorProvider>
+  );
+};
+
+
+export default App;
