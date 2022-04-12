f73651951bd1cea756ed847081fc4692b01f5869
woosang <woosang3078@hanmail.net>
2022-04-12 19:12:27 +0900

    Implement counter by redux code

---
+++ implement counter/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import CounterContainer from './containers/CouterContainer';
+
+function App() {
+  return (
+    <div>
+      <CounterContainer />
+    </div>
+  );
+}
+
+export default App;
