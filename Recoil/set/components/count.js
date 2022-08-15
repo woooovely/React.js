bf3cb5c1227dea1649514ddb691e63b33c1063e7
woosang <woosang3078@hanmail.net>
2022-08-15 19:54:14 +0900

    :: pratice set of recoil

---
+++ Recoil/set/components/count.js
@@ -0,0 +1,22 @@
+import { atom, selector } from 'recoil';
+
+export const countState = atom({
+    key: 'countState',
+    default: 0,
+})
+
+export const inputState = atom({
+    key: 'inputState',
+    default: '',
+})
+
+export const countInputState = selector({
+    key: 'countInputState',
+    get: ({ get }) => {
+        return `현재 카운트는 ${get(countState)}이고 입력값은 ${get(inputState)} 이다.`;
+    },
+    set: ({ set }, newValue) => {
+        set(countState, Number(newValue));
+        set(inputState, newValue + '');
+    }
+})
